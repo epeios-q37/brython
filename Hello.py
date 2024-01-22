b8780f133338bbc0e40684f14af1f93196940eec
epeios-q37 <epeios-q37@users.noreply.github.com>
2024-01-22 17:32:13 +0100

    22/01/2024 17:32:13

--- Hello.py
+++ Hello.py
@@ -1,30 +1,20 @@
 import atlastk
 
-text = ""
 
 async def acConnect(dom):
-  # dom.debugLog()
-  print("acConnect")
   await dom.inner("", BODY)
   await dom.focus("input")
 
 
 async def acSubmit(dom):
-  global text
-  text = await dom.getValue("input")
-  atlastk.broadcastAction("Display")
+  await dom.setValue("output", f"Hello, {await dom.getValue('input')}!")
   await dom.setValue("input", "")
   await dom.focus("input")
-
-
-async def acDisplay(dom):
-  await dom.setValue("output", f"Hello, {text}!")
 
 
 CALLBACKS = {
   "": acConnect,
   "Submit": acSubmit,
-  "Display": acDisplay
 }
 
 
